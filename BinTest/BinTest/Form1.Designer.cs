332b58ac95816bc973959ecfbcdda08baba23601
Martin Syrový <syrovym.05@spst.eu>
2023-04-16 22:10:16 +0200

    I have it

--- BinTest/BinTest/Form1.Designer.cs
+++ BinTest/BinTest/Form1.Designer.cs
@@ -40,46 +40,65 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 176);
+            this.textBox1.Location = new System.Drawing.Point(15, 36);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(120, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(160)))), ((int)(((byte)(133)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(140)))), ((int)(((byte)(133)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(150)))), ((int)(((byte)(133)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button1.Location = new System.Drawing.Point(172, 60);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.Size = new System.Drawing.Size(146, 27);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Nastavit (Nový záznam)";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.TabStop = false;
+            this.button1.Text = "Nový záznam";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(52, 63);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(88, 20);
             this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(52, 215);
+            this.numericUpDown2.Location = new System.Drawing.Point(15, 79);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             5,
             0,
@@ -101,7 +120,7 @@
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(183, 215);
+            this.numericUpDown3.Location = new System.Drawing.Point(146, 79);
             this.numericUpDown3.Maximum = new decimal(new int[] {
             5,
             0,
@@ -124,166 +143,209 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 47);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(49, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(93, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Počet známek";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 199);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(37, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Váha";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(180, 199);
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(143, 59);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.Size = new System.Drawing.Size(57, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Známka";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(180, 160);
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(143, 16);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.Size = new System.Drawing.Size(57, 17);
             this.label4.TabIndex = 12;
-            this.label4.Text = "Prijemni";
+            this.label4.Text = "Příjmení";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(49, 160);
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 16);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.Size = new System.Drawing.Size(48, 17);
             this.label5.TabIndex = 11;
-            this.label5.Text = "¨Jméno";
+            this.label5.Text = "Jméno";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(183, 176);
+            this.textBox2.Location = new System.Drawing.Point(146, 36);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(120, 20);
             this.textBox2.TabIndex = 13;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(228, 256);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(160)))), ((int)(((byte)(133)))));
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(140)))), ((int)(((byte)(133)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(150)))), ((int)(((byte)(133)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(77, 125);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(129, 27);
             this.button2.TabIndex = 14;
-            this.button2.Text = "Zadat";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.TabStop = false;
+            this.button2.Text = "Zadat ";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(492, 60);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(210, 225);
-            this.listBox1.TabIndex = 15;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(590, 291);
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(160)))), ((int)(((byte)(133)))));
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(140)))), ((int)(((byte)(133)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(150)))), ((int)(((byte)(133)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(897, 294);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.Size = new System.Drawing.Size(128, 27);
             this.button3.TabIndex = 17;
-            this.button3.Text = "Zapsat do souboru";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.TabStop = false;
+            this.button3.Text = "Vypsat záznamy";
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(366, 60);
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(425, 63);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(120, 225);
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(122, 225);
             this.textBox3.TabIndex = 18;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(407, 44);
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(460, 43);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.Size = new System.Drawing.Size(48, 17);
             this.label6.TabIndex = 19;
-            this.label6.Text = "¨Jméno";
+            this.label6.Text = "Jméno";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(569, 44);
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(606, 43);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.Size = new System.Drawing.Size(57, 17);
             this.label7.TabIndex = 20;
             this.label7.Text = "Známky";
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(807, 63);
+            this.listBox2.Location = new System.Drawing.Point(828, 63);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(197, 225);
             this.listBox2.TabIndex = 21;
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(851, 44);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(134, 13);
-            this.label8.TabIndex = 22;
-            this.label8.Text = "Kontrola - výpis ze souboru";
-            // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(867, 31);
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(869, 43);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(88, 13);
+            this.label9.Size = new System.Drawing.Size(112, 17);
             this.label9.TabIndex = 23;
             this.label9.Text = "Vysledny zaznam";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(34, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 176);
+            this.panel1.TabIndex = 24;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(738, 63);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(59, 225);
+            this.listBox3.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(740, 43);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 17);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Pruměr";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(553, 63);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(179, 225);
+            this.listBox4.TabIndex = 27;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1061, 380);
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1061, 350);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown3);
-            this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -302,14 +364,16 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox4;
     }
 }
 
--- BinTest/BinTest/Form1.cs
+++ BinTest/BinTest/Form1.cs
@@ -16,91 +16,112 @@ namespace BinTest
         public Form1()
         {
             InitializeComponent();
+            this.Text = "TestBinarniSoubory";
+            this.ShowIcon = false;
+            this.CenterToScreen();
+            button2.Visible = false;
+            panel1.Visible = false;
+            //FileStream fs = new FileStream("seznam.dat", FileMode.Truncate, FileAccess.Write);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            button2.Visible = false;
+            if (File.Exists("seznam.dat")) 
+            {                
+                File.Delete("seznam.dat");
+            }
         }
-      
+
         int pocetznamek;
         int i = 0;
         string s = "";
+        string znamky = "";
         int pocet = 0;
         int soucet = 0;
         int znamka = 0;
         int vaha = 0;
+
         private void button1_Click(object sender, EventArgs e)
         {
             pocetznamek = (int)numericUpDown1.Value;
-            i = 0;
+            i = 0;                        
             s = pocetznamek.ToString() + " ";
-            button2.Visible = true;
             pocet = 0;
             soucet = 0;
+
+            button2.Visible = true;
+            panel1.Visible = true;
+            numericUpDown2.Value = 1;
+            numericUpDown3.Value = 1;
+            textBox1.Text = "";
+            textBox2.Text = "";
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            i++;
-           
-            if (i <= pocetznamek*2)
-            {
-                
-                if (i % 2 == 0)
-                {
-                    znamka = (int)numericUpDown3.Value;
-                    s += znamka.ToString()+ " ";
-                   
-                    soucet += znamka * vaha;
-                    pocet += vaha;
-                }
-                else
-                {
-                    vaha = (int)numericUpDown2.Value;
-                    s += vaha.ToString() + " ";
-                }
-                
-                
+            i++;           
+            if (i <= pocetznamek)
+            {                
+                vaha = (int)numericUpDown2.Value;
+                s += vaha.ToString() + " ";
+
+                znamka = (int)numericUpDown3.Value;
+                s += znamka.ToString() + " ";
+                znamky += znamka.ToString() + " ";
+
+                soucet += znamka * vaha;
+                pocet += vaha;
             }
-            else
-            {
-                listBox1.Items.Add(s);
-                double prumer = (double)(soucet) / (double)(pocet);
-                if (prumer == 5)
+            
+            if(i == pocetznamek)
+            {                
+                listBox4.Items.Add(znamky);              
+                   
+                double prumer = Math.Round( (double)(soucet) / (double)(pocet),2);                
+                listBox3.Items.Add(prumer);
+                string jmeno = textBox1.Text + " " + textBox2.Text;
+
+                if (prumer >= 4.5)
                 {
                     textBox3.Text += "John Doe" + Environment.NewLine;
+                    jmeno = "John Doe";
+                    MessageBox.Show("Pozor!! Propadáš, vychází ti 5!!", "Upozornění", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
                 else textBox3.Text += textBox1.Text + " " + textBox2.Text + Environment.NewLine;
 
+                if (prumer < 1.5) MessageBox.Show("Gratulace!! Vycházi ti jednicka", "Upozornění", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (prumer > 3.5 && prumer < 4.5) MessageBox.Show("Pozor!! Vychazi ti 4", "Upozornění", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Zapis(s +jmeno);
                 button2.Visible = false;
-
-                if (prumer < 1.5) MessageBox.Show("Gratulace!! Vycházi ti jednicka");
-                if (prumer > 3.5 && prumer < 4.5) MessageBox.Show("Pozor!! Vychazi ti 4");
-
+                znamky = "";
             }                        
+        }
+
+        void Zapis(string s)
+        {
+            FileStream fs = new FileStream("seznam.dat", FileMode.Append, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs, Encoding.UTF8);
+            bw.Write(s);            
+            fs.Close();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream("seznam.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            BinaryWriter bw = new BinaryWriter(fs, Encoding.UTF8);
+            FileStream fs = new FileStream("seznam.dat", FileMode.Open, FileAccess.Read);            
             BinaryReader br = new BinaryReader(fs, Encoding.UTF8);
-            int i = 0;
-            foreach(string s in listBox1.Items)
-            {
-                bw.Write(s + textBox3.Lines[i]);
-                i++;
-            }
-
+            listBox2.Items.Clear();
+           
             br.BaseStream.Position = 0;
             while(br.BaseStream.Position < br.BaseStream.Length)
             {
                 listBox2.Items.Add(br.ReadString());
             }
 
-
-
+            fs.Close();
         }
+
+       
     }
 }
